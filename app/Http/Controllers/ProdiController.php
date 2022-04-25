c40cd9ab1052167ca7d469dc92ddebc14f3d12aa
Muhammad Naufal Anugrah <100113738+MNAG19@users.noreply.github.com>
2022-04-25 12:13:51 +0700

    create prodi

--- app/Http/Controllers/ProdiController.php
+++ app/Http/Controllers/ProdiController.php
@@ -7,42 +7,40 @@ use App\Models\Programstudi;
 
 class ProdiController extends Controller
 {
-    //Tugas:
-    //1.Tampilkan data prodi di dalam Tabel
-    //2.Tampilkan tomobol aksi edit dan hapus
+    //Tugas : 
+    //Tampilkan data Program Studi di dalam Tabel
+    //Tampilkan tombol aksi (action) edit dan hapus
+
     public function index(){
         $programstudi = Programstudi::all();
-        $kampus = "Universitas Multi Data Palembang";
-        return view('programstudi.index', compact('programstudi', 'kampus'));
+        $kampus = "Universitas MDP";
+        return view("programstudi.index", compact('programstudi', 'kampus'));
     }
-    
+
     public function create()
     {
-        $kampus = "Universitas Multi Data Palembang";
-        return view("programstudi.create", compact('kampus'));
+       $kampus = "Universitas MDP";
+       return view("programstudi.create", compact('kampus'));
     }
 
     public function store(Request $request)
     {
-        //lakukan validation
+        //0. Lakukan Validation
         $validation = $request->validate([
-            'nama' => 'required|min:5|max:20',
-            //field dan aturan lainnya
+           'nama' => 'required|min:5|max:20',
+           //field dan atauran lainnya
         ]);
 
-        //1 ambil nilai inputen form
-
-        //panggil fungsi insert - boleh raw/eloquent
+        //1. ambil nilai inputan form
+        //2. panggil fungsi insert - boleh raw / eloquent
         $programstudi = new Programstudi();
         $programstudi->nama_prodi = $request->nama; // $validation['nama']
-        $programstudi->kode_prodi = $request->kode;
+        $programstudi->kode_prodi = $request->kode; 
         $programstudi->id_fakultas = 1;
-        $programstudi->alamat = "Jl. Rajawali";
         $programstudi->save();
 
-        //3 redirect ke halaman index / detail / form create
-        $request->session()->flash("info", "Data prodi $request->nama berhasil disimpan");
+        //3. redirect ke halaman index / detail / form create
+        $request->session()->flash("info", "Data prodi $request->nama berhasil disimpan!");
         return redirect()->route("programstudi.create");
     }
-
 }
